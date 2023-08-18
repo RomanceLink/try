e1af0f0eb5650ef53ba55425c6a81395fbd9f943
panlong <pllovelwj@gmail.com>
2023-08-18 21:11:34 +0800

    Initial Commit

---
+++ try/tryApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryApp.swift
+//  try
+//
+//  Created by pan long on 2023/8/18.
+//
+
+import SwiftUI
+
+@main
+struct tryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
